2819bb0a53d9b77aa3c3c58d581f20bb21bcdc91
janth96 <janth1996@gmail.com>
2021-05-11 23:36:27 +1200

    Install Sass
    - Installed sass package
    - Installed sass-loader@10.1.1 (newest version wasn't compatible)
    - Installed webpack
    - Created app.scss with ITCSS heirarchy

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './components/ui/App.vue'
 import store from "./store/store"
 import router from "./router"
+import "./assets/sass/app.scss"
 
 const app = createApp(App)
 app.config.productionTip = false
