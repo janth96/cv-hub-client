3dd1bf3997fd0addef0f09680311af7cd0872eec
janth96 <janth1996@gmail.com>
2021-05-30 01:58:15 +1200

    Minor style improvements
    - Added styling for navbar
    - Added transition for navbar

--- src/components/ui/Nav.vue
+++ src/components/ui/Nav.vue
@@ -1,14 +1,20 @@
 <template lang="html">
   <nav class="top__bar">
-    <ul v-if="token != null">
-      <li><router-link :to="{ name: 'account' }">Account</router-link></li>
-      <li><router-link :to="{ name: 'home' }">Home</router-link></li>
-      <li><router-link :to="{ name: 'logout' }">Logout</router-link></li>
-    </ul>
-    <ul v-else>
-      <li><router-link :to="{ name: 'register' }">Register</router-link></li>
-      <li><router-link :to="{ name: 'login' }">Login</router-link></li>
-    </ul>
+    <!-- TODO logo -->
+    <!-- Some flashy logo... -->
+    <p class="logo"><router-link :to="{ name: 'home' }">Some flashy logo</router-link></p>
+    <transition name="slide-bottom-to-top" mode="out-in">
+      <div class="auth__nav" v-if="token != null">
+        <router-link :to="{ name: 'account' }">Account</router-link>
+        <router-link :to="{ name: 'logout' }">Logout</router-link>
+      </div>
+      <div class="auth__nav" v-else>
+        <router-link :to="{ name: 'register' }">Register</router-link>
+        <router-link :to="{ name: 'login' }">Login</router-link>
+      </div>
+    </transition>
+    <!-- <transition name="slide-bottom-to-top" mode="out-in"> -->
+    <!-- </transition> -->
   </nav>
 </template>
 
